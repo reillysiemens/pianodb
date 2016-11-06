82962c3a126b9059ffe146a42f5c5353aa8cc781
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2016-11-05 23:07:19 -0700

    Use parametrized database fixture for testing

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,32 +1,38 @@
 import os
+from urllib.parse import urlparse
+import pytest
 from playhouse.db_url import connect
 from pianodb.pianodb import create_database
 
 
-def test_create_sqlite():
-    # Setup the database.
-    db_path = '/tmp/piano-test.db'
-    db_uri = "sqlite:///{}".format(db_path)
-    database = connect(db_uri)
+@pytest.fixture(scope='module', params=[
+    'sqlite:////tmp/pianodb-test.db',
+    'mysql://root@127.0.0.1:3306/pianodb_test',
+    'postgres://postgres@127.0.0.1:5432/pianodb_test',
+])
+def database(request):
+    """
+    A fixture for setting up and tearing down SQLite, MySQL, and PostgreSQL
+    databases using ``playhouse.db_url.connect``.
+
+    TODO:
+        Figure out how to teardown MySQL and PostgreSQL databases.
+
+    Note:
+        ``playhouse.db_url.connect`` requires four forward slashes in the SQLite
+        URI for some reason.
+    """
+    database = connect(request.param)
+
+    yield database
+
+    if 'sqlite' in request.param:
+        parsed_uri = urlparse(request.param)
+        os.remove(parsed_uri.path)
+
+
+def test_pianodb_can_create_database(database):
+    """
+    Test that ``pianodb`` can create a database.
+    """
     create_database(database)
-
-    # Teardown the database.
-    os.remove(db_path)
-
-
-def test_create_mysql():
-    # Setup the database.
-    db_uri = 'mysql://root@127.0.0.1:3306/pianodb_test'
-    database = connect(db_uri)
-    create_database(database)
-
-    # TODO: Figure out how to "tear down" the MySQL database...
-
-
-def test_create_postgresql():
-    # Setup the database.
-    db_uri = 'postgres://postgres@127.0.0.1:5432/pianodb_test'
-    database = connect(db_uri)
-    create_database(database)
-
-    # TODO: Figure out how to "tear down" the PostgreSQL database...
